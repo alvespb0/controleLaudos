8c1df24354ac76ac7807e766231c8857848827ab
alvespb0 <alvespb0@gmail.com>
2025-04-22 14:20:36 -0300

    feat(laudosView): feito pagina de listagem de laudos

--- app/Models/Laudo.php
+++ app/Models/Laudo.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Cliente;
+use App\Models\Op_Comercial;
+use App\Models\Op_Tecnico;
+use App\Models\Status;
+
 class Laudo extends Model
 {
     protected $table = 'laudos';
@@ -21,4 +26,21 @@ class Laudo extends Model
         'cliente_id',
         'comercial_id',
     ];
+
+    public function cliente(){
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function comercial(){
+        return $this->belongsTo(Op_Comercial::class);
+    }
+
+    public function tecnico(){
+        return $this->belongsTo(Op_Tecnico::class);
+    }
+    
+    public function status(){
+        return $this->belongsTo(Status::class);
+    }
+
 }
