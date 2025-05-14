23a7b9922a065a88cfb69543af10a8ff4c983eb1
alvespb0 <alvespb0@gmail.com>
2025-05-14 09:46:18 -0300

    style(form): alterado a estilização de todos os formulários

--- resources/views/Laudo/Laudo_edit.blade.php
+++ resources/views/Laudo/Laudo_edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="d-flex justify-content-center">
-    <div class="card shadow-lg w-100" style="max-width: 600px; border: none; background-color: var(--mid-color);">
+    <div class="card shadow-lg w-100" style="max-width: 600px; border: none;">
         <div class="card-body p-4">
             @if ($errors->any())
                 <div class="alert alert-danger">
@@ -17,26 +17,26 @@
             <form action="{{ route('update.laudo', $laudo->id) }}" method="POST">
                 @csrf
                 <div class="mb-3">
-                    <label for="nome" class="form-label">Laudo</label>
+                    <i class="bi bi-file-earmark-text"></i><label for="nome" class="form-label">&nbspLaudo</label>
                     <input type="text" class="form-control" id="nome" name="nome" placeholder="nome ou breve descrição do laudo" value="{{$laudo->nome}}" required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="dataPrevisao">Data de Previsao de Conclusao de Laudo <small class="text-muted">* Opcional</small></label>
+                    <i class="bi bi-calendar3"></i><label for="dataPrevisao">&nbspData de Previsao de Conclusao de Laudo <small class="text-muted">* Opcional</small></label>
                     <input type="date" name="dataPrevisao" id="dataPrevisao" value="{{$laudo->data_previsao}}" class="form-control">
                 </div>
 
                 <div class="mb-3">
-                    <label for="dataAceiteContrato">Data de Aceite do Contrato</label>
+                    <i class="bi bi-calendar3"></i><label for="dataAceiteContrato">&nbspData de Aceite do Contrato</label>
                     <input type="date" name="dataAceiteContrato" id="dataAceiteContrato" class="form-control" value="{{$laudo->data_aceite}}" required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="dataFimContrato">Data de fim de contrato</label>
+                    <i class="bi bi-calendar3"></i><label for="dataFimContrato">&nbspData de fim de contrato</label>
                     <input type="date" name="dataFimContrato" id="dataFimContrato" class="form-control" value="{{$laudo->data_fim_contrato}}" required>
                 </div>
                 <div class="mb-3">
-                    <label for="cliente">Cliente</label>
+                    <i class="bi bi-person"></i><label for="cliente">&nbspCliente</label>
                     <select name="cliente" id="cliente" class = "form-control" required>
                         @foreach($clientes as $cliente)
                             <option value="{{ $cliente->id }}" {{ $cliente->id == $laudo->cliente_id ? 'selected' : '' }}>{{ $cliente->nome }}</option>
@@ -44,7 +44,7 @@
                     </select>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label d-block">Esocial?</label>
+                    <label class="form-label d-block"><i class="bi bi-journal-bookmark-fill"></i>&nbspEsocial?</label>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="esocial" id="esocial_sim" value="1" {{$laudo->esocial ? 'checked' : ''}} required>
                         <label class="form-check-label" for="esocial_sim">Sim</label>
@@ -56,12 +56,12 @@
                 </div>
 
                 <div class="mb-3">
-                    <label for="numFunc">Número de funcionários</label>
+                    <i class="bi bi-123"></i><label for="numFunc">&nbspNúmero de funcionários</label>
                     <input type="number" name="numFuncionarios" class="form-control" id="" placeholder="insira o numero de funcionários" min=1 required value="{{$laudo->numero_clientes}}">
                 </div>
 
                 <div class="mb-3">
-                    <label for="Vendedor">Vendedor</label>
+                    <i class="bi bi-person"></i><label for="Vendedor">&nbspVendedor</label>
                     <select name="comercial" id="Vendedor" class = "form-control" required>
                         @foreach($comerciais as $comercial)
                             <option value="{{$comercial->id}}" {{ $comercial->id == $laudo->comercial->id ? 'selected' : '' }}>{{$comercial->usuario}}</option>
@@ -69,7 +69,7 @@
                     </select>
                 </div>
                 <div class="text-center">
-                    <button type="submit" class="btn btn-primary px-4">Editar</button>
+                    <button type="submit" class="btn btn-primary col-sm-12">Editar</button>
                 </div>
             </form>
         </div>
--- resources/views/Status/Status_edit.blade.php
+++ resources/views/Status/Status_edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="d-flex justify-content-center">
-    <div class="card shadow-lg w-100" style="max-width: 600px; border: none; background-color: var(--mid-color);">
+    <div class="card shadow-lg w-100" style="max-width: 600px; border: none;">
         <div class="card-body p-4">
             @if ($errors->any())
                 <div class="alert alert-danger">
@@ -17,17 +17,17 @@
             <form action="{{ route('update.status', $Status->id) }}" method="POST">
                 @csrf
                 <div class="mb-3">
-                    <label for="status" class="form-label">Status</label>
+                    <i class="bi bi-patch-check"></i><label for="status" class="form-label">&nbspStatus</label>
                     <input type="text" class="form-control" id="status" name="nome" required placeholder="Nome do Status" value = "{{$Status->nome}}">
                 </div>
 
                 <div class="mb-3">
-                    <label for="cor">Cor do Status</label>
+                    <i class="bi bi-brilliance"></i><label for="cor">&nbspCor do Status</label>
                     <input type="color" name="cor" class="form-control" id="cor" value="{{$Status->cor}}">
                 </div>
 
                 <div class="text-center">
-                    <button type="submit" class="btn btn-primary px-4">Editar</button>
+                    <button type="submit" class="btn btn-primary col-sm-12">Editar</button>
                 </div>
             </form>
         </div>
