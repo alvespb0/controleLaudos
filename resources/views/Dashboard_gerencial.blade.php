603f9ddb55edd7604ae5f13180a80daed489d6b1
alvespb0 <alvespb0@gmail.com>
2025-06-09 11:42:44 -0300

    fix(files): adicionado os indicadores de files

--- resources/views/Dashboard_gerencial.blade.php
+++ resources/views/Dashboard_gerencial.blade.php
@@ -6,28 +6,51 @@
 
     <div class="row justify-content-center">
         <div class="col-md-4 d-flex justify-content-center mb-4">
-            <div style="max-width: 400px; width: 100%;">
-                {!! $chartStatus->container() !!}
+            <div class="border p-3 shadow-sm rounded" style="width: 100%; max-width: 400px;">
+                <h5 class="text-center mb-3">Laudos por status</h5>
+                <div style="height: 300px;">
+                    {!! $chartStatus->container() !!}
+                </div>
+            </div>
+        </div>
+    
+        <div class="col-md-4 d-flex justify-content-center mb-4">
+            <div class="border p-3 shadow-sm rounded" style="width: 100%; max-width: 400px;">
+                <h5 class="text-center mb-3">Laudos por técnico</h5>
+                <div style="height: 300px;">
+                    {!! $chartTecnico->container() !!}
+                </div>
             </div>
         </div>
 
         <div class="col-md-4 d-flex justify-content-center mb-4">
-            <div style="max-width: 400px; width: 100%;">
-                {!! $chartTecnico->container() !!}
+            <div class="border p-3 shadow-sm rounded" style="width: 100%; max-width: 400px;">
+                <h5 class="text-center mb-3">Laudos por vendedor</h5>
+                <div style="height: 300px;">
+                    {!! $chartVendedor->container() !!}
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-4 d-flex justify-content-center mb-4">
+            <div class="border p-3 shadow-sm rounded" style="width: 100%; max-width: 400px;">
+                <h5 class="text-center mb-3">Clientes Novos X Renovações</h5>
+                <div style="height: 300px;">
+                    {!! $chartClientes->container() !!}
+                </div>
             </div>
         </div>
 
         <div class="col-md-4 d-flex justify-content-center mb-4">
-            <div style="max-width: 400px; width: 100%;">
-                {!! $chartVendedor->container() !!}
+            <div class="border p-3 shadow-sm rounded" style="width: 100%; max-width: 400px;">
+                <h5 class="text-center mb-3">Orçamentos por Mês</h5>
+                <div style="height: 300px;">
+                    {!! $chartOrcamentos->container() !!}
+                </div>
             </div>
         </div>
 
-        <div class="col-md-4 d-flex justify-content-center mb-4">
-            <div style="max-width: 400px; width: 100%;">
-                {!! $chartClientes->container() !!}
-            </div>
-        </div>
     </div>
 
     {{-- Carrega Chart.js --}}
@@ -36,6 +59,7 @@
     {!! $chartTecnico->script() !!}
     {!! $chartVendedor->script() !!}
     {!! $chartClientes->script() !!}
+    {!! $chartOrcamentos->script() !!}
 </div>
 
 @endsection
