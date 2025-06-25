6b84c1a4e1abd79fd7acd389831a96c566d31ddd
alvespb0 <alvespb0@gmail.com>
2025-06-25 10:58:25 -0300

    feat(documentos): feito migration model e controller da classe de documentos_tecnicos

---
+++ app/Http/Controllers/Documentos_TecnicosController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Documentos_TecnicosController extends Controller
+{
+    //
+}
---
+++ database/migrations/2025_06_25_133733_create_documentos_tecnicos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documentos_tecnicos', function (Blueprint $table) {
+            $table->id();
+            $table->enum('tipo_documento', ['CAT', 'PPP', 'ADENDO']);
+            $table->string('descricao');
+            $table->date('data_elaboracao');
+            $table->unsignedBigInteger('cliente_id')->nullable();
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->unsignedBigInteger('tecnico_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('cliente_id')->references('id')->on('cliente')->nullOnDelete();
+            $table->foreign('status_id')->references('id')->on('status')->nullOnDelete();
+            $table->foreign('tecnico_id')->references('id')->on('op_tecnicos')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documentos_tecnicos');
+    }
+};
