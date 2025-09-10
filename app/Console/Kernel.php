03c0dc6bb8b3ba3adeff01a852131f0e129ae349
alvespb0 <alvespb0@gmail.com>
2025-09-10 07:45:34 -0300

    hotfix(kernel): descomentado linha da schedule de proximo contato

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        #$schedule->job(new NotificarVendedoresProximoContato)->dailyAt('08:00');
+        $schedule->job(new NotificarVendedoresProximoContato)->dailyAt('08:00');
         $schedule->job(new NotificarVendedorValidadeLaudo)->dailyAt('08:00');
     }
 
